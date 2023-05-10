c50a36ea17c94ce8aed67a94f5df3dd55ffae628
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-10 14:45:37 +0530

    ..

--- frontend/src/components/user/Managevideos.js
+++ frontend/src/components/user/Managevideos.js
@@ -8,6 +8,109 @@ const Managevideos = () => {
 
   const { apiUrl } = app_config;
 
+  const convertVideotoBlog = async (id, index) => {
+    setSelBlog(index);
+    console.log(id);
+    // setTimeout(() => {
+    //   console.log(id);
+    //   setSelBlog(null);
+    // }, 3000);
+    // return;
+    // setBlogLoading(true);
+    const response = await fetch(url + "/util/transcribe/" + id);
+    console.log(response.status);
+    if (response.status === 200) {
+      Swal.fire({
+        title: "Success",
+        text: "Video converted to blog",
+        icon: "success",
+        timer: 2000,
+        showConfirmButton: false,
+      });
+      const data = await response.json();
+      console.log(data);
+      // getDataFromBackend();
+      
+      navigate("/blog/addblog/" + id);
+      // setBlogLoading(false);
+    } else if (response.status === 400) {
+      toast.error("Video is not transcribed yet");
+      // setBlogLoading(false);
+    } else if (response.status === 500) {
+      toast.error("Internal server error");
+      // setBlogLoading(false);
+    }
+    setSelBlog(null);
+  };
+
+  const deleteVideo = async (id) => {
+    console.log(id);
+    const response = await fetch(url + "/video/deletebyid/" + id, {
+      method: "Delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 200) {
+      toast.success("Video Deleted Successfully");
+      getDataFromBackend();
+    }
+  };
+
+  const displayVideo = () => {
+    if (!loading) {
+      return userArray.map(({ _id, title, description, file, thumbnail }, index) => (
+        <div className="col-md-3 mt-4" key={_id}>
+            <div
+              className="thumb-small"
+              style={{ backgroundImage: `url('${thumbnail ?url + "/" + thumbnail: 'video-placeholder.webp'}')` }}
+            >
+              <div className="p-3 thumb-options">
+                {/* <h5 className="card-title">{title}</h5>
+              <p className="text-muted">{description}</p> */}
+                <Link to={"/user/viewvideo/" + _id}>
+                  <button className="btn btn-primary btn-floating">
+                    <i class="fas fa-eye "></i>
+                  </button>
+                </Link>
+                &nbsp;&nbsp;&nbsp;
+                <button
+                  className="btn btn-danger btn-floating"
+                  onClick={(e) => deleteVideo(_id)}
+                >
+                  <i class="fas fa-trash"></i>
+                </button>
+                &nbsp;&nbsp;&nbsp;
+                {!blogLoading ? (
+                  <button
+                    className={"btn btn-secondary "+(selBlog===index?'btn-rounded':'btn-floating')}
+                    onClick={(e) => convertVideotoBlog(_id, index)}
+                  ><i className={"fa-solid fa-gear "+(selBlog===index?'fa-spin':'')}></i>
+                    {selBlog===index?' Converting ...':'' }
+                  </button>
+                ) : (
+                  <button className="btn btn-success" disabled>
+                    Converting...
+                  </button>
+                )}
+              </div>
+
+              <p className="h3 text-muted ms-3">{title}</p>
+              <p className="h6 text-muted ms-3">{file}</p>
+            </div>
+          
+        </div>
+      ));
+    } else {
+      return (
+        <div className="text-center">
+          {/* <Loading></Loading> */}
+          <h1>Loading ...</h1>
+        </div>
+      );
+    }
+  };
+
   const saveVideo = async (file) => {
     const res = await fetch('http://localhost:5000/user/add', {
       method: 'POST',
